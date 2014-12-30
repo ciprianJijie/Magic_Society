90b2057ade635aa0e558010e8cc20e7c561d9ae8
Ladislao Lainez <ladislao.lainez@plungeinteractive.com>
2014-12-30 01:46:42 +0100

    Basic code utils.

---
+++ Assets/Scripts/Core/Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MS
+{
+	public class NoInstance : Exception
+	{
+		public NoInstance(object obj)
+			: base ("No instance initialized for " + obj)
+		{
+		}
+	}
+	
+	public class AlreadyInstantiated : Exception
+	{
+		public AlreadyInstantiated(object obj)
+			: base ("Singleton " + obj + " already instantiated")
+		{
+		}
+	}
+}
---
+++ Assets/Scripts/Utils/Debug.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MS
+{
+	namespace Debug
+	{
+		public static class Core
+		{
+			[System.Diagnostics.Conditional("DEBUG_CORE")]
+			public static void Log(string message)
+			{
+				UnityEngine.Debug.Log (message);
+			}
+
+			[System.Diagnostics.Conditional("DEBUG_CORE")]
+			public static void LogError(string message)
+			{
+				UnityEngine.Debug.LogError(message);
+			}
+		}
+	}
+}
+
