69d94db56c219e7075ba255ee39780eda3da96cf
Ladislao Lainez <ladislao.lainez@plungeinteractive.com>
2014-12-31 20:11:41 +0100

    Singleton manager created to control general game states.

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using MS.Model;
+
+namespace MS.Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+        #region Properties
+        public static GameManager Instance
+        {
+            get
+            {
+                if (m_instance == null)
+                {
+                    throw new NoInstance(null);
+                }
+                return m_instance;
+            }
+        }
+
+        #endregion
+
+        #region Monobehaviour methods
+        void Awake()
+        {
+            if (m_instance != null)
+            {
+                throw new AlreadyInstantiated(this);
+            }
+            MS.Debug.Core.Log("Game Manager singleton instantiated.");
+
+            m_instance = this;
+
+            OnStart();
+        }
+
+        void OnDestroy()
+        {
+            OnFinish();
+        }
+
+        #endregion
+
+        #region Events methods
+
+        protected void OnStart()
+        {
+        }
+
+        protected void OnFinish()
+        {
+        }
+
+        #endregion
+
+        #region Attributes
+
+        private static  GameManager     m_instance;
+
+        #endregion
+    }	
+}
