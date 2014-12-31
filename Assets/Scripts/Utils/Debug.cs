6e9d07b146de50adf5757709e9cef21fe6ae6334
Ladislao Lainez <ladislao.lainez@plungeinteractive.com>
2014-12-31 20:06:10 +0100

    Improve usage of Log methods.

--- Assets/Scripts/Utils/Debug.cs
+++ Assets/Scripts/Utils/Debug.cs
@@ -8,13 +8,13 @@ namespace MS
 		public static class Core
 		{
 			[System.Diagnostics.Conditional("DEBUG_CORE")]
-			public static void Log(string message)
+			public static void Log(object message)
 			{
 				UnityEngine.Debug.Log (message);
 			}
 
 			[System.Diagnostics.Conditional("DEBUG_CORE")]
-			public static void LogError(string message)
+			public static void LogError(object message)
 			{
 				UnityEngine.Debug.LogError(message);
 			}
