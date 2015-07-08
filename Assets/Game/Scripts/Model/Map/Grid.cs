678263cedd2665e610e8df5273e2d2100b3623e1
Ladislao Lainez <ladislao.lainez@plungeinteractive.com>
2015-07-08 13:32:23 +0200

    Add support for Grid parsing from/to JSON.

--- Assets/Game/Scripts/Model/Map/Grid.cs
+++ Assets/Game/Scripts/Model/Map/Grid.cs
@@ -1,20 +1,66 @@
-﻿using UnityEngine;
-using System.Collections;
+using SimpleJSON;
 
 namespace MS
 {
     public class Grid : ModelElement
     {
-        public override void FromJSON(SimpleJSON.JSONNode node)
+        public override void FromJSON(JSONNode node)
         {
-            throw new System.NotImplementedException();
+            int hSize;
+            int vSize;
+
+            hSize = node["size"]["horizontal"].AsInt;
+            vSize = node["size"]["vertical"].AsInt;
+
+            m_Tiles = new Tile[hSize, vSize];
+
+            int x;
+            int y;
+            Tile tile;
+
+            x = 0;
+            foreach (JSONNode row in node["tiles"].AsArray)
+            {
+                y = 0;
+                foreach (JSONNode tileNode in row.AsArray)
+                {
+                    tile = new Tile();
+
+                    tile.FromJSON(tileNode);
+
+                    m_Tiles[x, y] = tile;
+
+                    y++;
+                }
+                x++;
+            }
         }
 
-        public override SimpleJSON.JSONNode ToJSON()
+        public override JSONNode ToJSON()
         {
-            throw new System.NotImplementedException();
+            JSONNode json = new JSONNode();
+            JSONArray tilesArray;
+
+            json["size"]["horizontal"]  =   m_Tiles.Length.ToString();
+            json["size"]["vertical"]    =   m_Tiles.GetLength(0).ToString();
+
+            tilesArray = new JSONArray();
+
+            for (int x = 0; x < m_Tiles.Length; x++)
+            {
+                for (int y = 0; y < m_Tiles.GetLength(0); y++)
+                {
+                    tilesArray.Add(m_Tiles[x,y].ToJSON());
+                }
+            }
+
+            json["tiles"] = tilesArray;
+
+            return json;
         }
 
         public string ID;
+
+        protected Tile[,] m_Tiles;
     }
 }
