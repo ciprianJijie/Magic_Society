ce258c08f67fbb475cc63b47ecd075155da4f8cc
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2015-07-24 17:20:51 +0200

    Adapt height system to new model representation.

--- Assets/Game/Scripts/View/World/TileView.cs
+++ Assets/Game/Scripts/View/World/TileView.cs
@@ -4,44 +4,46 @@ namespace MS
 {
 	public class TileView : View<Tile>
 	{
-        public Elevation FertileTilePrefab;
-        public Elevation BarrenTilePrefab;
-        public Elevation DesertTilePrefab;
-        public Elevation FrozeTilePrefab;
-        public Elevation VolcanicTilePrefab;
-
-        private Elevation m_InstantiatedTile;
-
-        public override void UpdateView()
-        {
-            if (m_InstantiatedTile != null)
-            {
-                Destroy(m_InstantiatedTile.gameObject);
-            }
-
-            Elevation tileToInstantiate;
-
-            switch (m_Model.TerrainType)
-            {
-                case Tile.Terrain.Barren:
-                    tileToInstantiate = BarrenTilePrefab;
-                    break;
-                case Tile.Terrain.Desert:
-                    tileToInstantiate = DesertTilePrefab;
-                    break;
-                case Tile.Terrain.Frozen:
-                    tileToInstantiate = FrozeTilePrefab;
-                    break;
-                case Tile.Terrain.Volcanic:
-                    tileToInstantiate = VolcanicTilePrefab;
-                    break;
-                default:
-                    tileToInstantiate = FertileTilePrefab;
-                    break;
-            }
-
-            m_InstantiatedTile = Utils.Instantiate<Elevation>(tileToInstantiate, this.transform, this.transform.position, this.transform.rotation);
-            m_InstantiatedTile.ChangeHeight(m_Model.Height);
+                public Elevation FertileTilePrefab;
+                public Elevation BarrenTilePrefab;
+                public Elevation DesertTilePrefab;
+                public Elevation FrozeTilePrefab;
+                public Elevation VolcanicTilePrefab;
+        
+                private Elevation m_InstantiatedTile;
+        
+                public override void UpdateView()
+                {
+                    if (m_InstantiatedTile != null)
+                    {
+                        Destroy(m_InstantiatedTile.gameObject);
+                    }
+        
+                    Elevation tileToInstantiate;
+        
+                    switch (m_Model.TerrainType)
+                    {
+                        case Tile.Terrain.Barren:
+                            tileToInstantiate = BarrenTilePrefab;
+                            break;
+                        case Tile.Terrain.Desert:
+                            tileToInstantiate = DesertTilePrefab;
+                            break;
+                        case Tile.Terrain.Frozen:
+                            tileToInstantiate = FrozeTilePrefab;
+                            break;
+                        case Tile.Terrain.Volcanic:
+                            tileToInstantiate = VolcanicTilePrefab;
+                            break;
+                        default:
+                            tileToInstantiate = FertileTilePrefab;
+                            break;
+                    }        
+                    m_InstantiatedTile = Utils.Instantiate<Elevation>(tileToInstantiate, this.transform, this.transform.position, this.transform.rotation);
+                    
+                    m_InstantiatedTile.ChangeHeight(m_Model.Height);
+                    
+                    
+                }
         }
-	}
 }
