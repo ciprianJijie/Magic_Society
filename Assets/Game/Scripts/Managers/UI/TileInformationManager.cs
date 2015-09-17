3ee364a58e5572c2e693e0d5abd29dde3820242b
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2015-09-17 13:01:57 +0200

    Resource information about tiles now uses the new Resource Amount views.

--- Assets/Game/Scripts/Managers/UI/TileInformationManager.cs
+++ Assets/Game/Scripts/Managers/UI/TileInformationManager.cs
@@ -9,9 +9,6 @@ namespace MS.Managers.UI
         public GameInputManager InputManager;
         public Controllers.UI.ResourceAmountController ResourceAmountController;
 
-        private float width;
-        private float height;
-
         public void UpdateInfo(int x, int y)
         {
             Model.Tile tile;
@@ -34,43 +31,8 @@ namespace MS.Managers.UI
             ResourceAmountController.Hide();
         }
 
-        protected Vector2 CalculatePosition()
-        {
-            Vector2 mousePosition;
-            Vector2 offset;
-            RectTransform rect;
-
-            mousePosition   =   Input.mousePosition;
-            rect            =   ResourceAmountController.Holder.gameObject.GetComponent<RectTransform>();
-            width           =   Mathf.Abs(rect.rect.width);
-            height          =   Mathf.Abs(rect.rect.height);
-
-            if (mousePosition.x < Screen.width / 2f)
-            {
-                offset.x = - width - 6f;
-            }
-            else
-            {
-                offset.x = 6f;
-            }
-
-            if (mousePosition.y < Screen.height / 2f)
-            {
-                offset.y = 6f;
-            }
-            else
-            {
-                offset.y = - height - 6f;
-            }
-
-            return new Vector2(mousePosition.x + offset.x, mousePosition.y + offset.y);
-        }
-
         protected void Start()
         {
-            width   =   Mathf.Abs(this.gameObject.GetComponent<RectTransform>().rect.width);
-            height  =   Mathf.Abs(this.gameObject.GetComponent<RectTransform>().rect.height);
-
             InputManager.OnTileHover        +=  UpdateInfo;
             InputManager.OnTileHoverEnds    +=  Hide;
 
