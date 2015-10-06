dd109d3f9cfaccbb4d33b4f1dcf52d0192516e99
Ladislao Lainez <thedragonladis@gmail.com>
2015-10-06 18:31:26 +0200

    Add new classes for new map representation.

---
+++ Assets/Game/Scripts/Model/World/Map/Region.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+using UnityEngine;
+
+namespace MS.Model.Map
+{
+    public class Region : ModelElement, IEnumerable<Area>
+    {
+        public Vector2          MapPosition; 
+
+        protected List<Area>    m_Areas;
+        protected Area          m_Capital;
+
+        public Region()
+        {
+            m_Areas = new List<Area>(7);
+
+            for (int i = 0; i < m_Areas.Count; i++)
+            {
+                m_Areas[i] = new Area();
+            }
+
+            m_Capital = m_Areas[0];
+        }
+
+        public override void FromJSON(JSONNode json)
+        {
+            base.FromJSON(json);
+
+            JSONArray array;
+
+            array = json["areas"].AsArray;
+
+            m_Capital.FromJSON(array[0]);
+
+            for (int i = 1; i < array.Count; i++)
+            {
+                m_Areas[i].FromJSON(array[i]);
+            }
+        }
+
+        public override JSONNode ToJSON()
+        {
+            JSONNode root;
+            JSONArray array;
+
+            root = base.ToJSON();
+            array = new JSONArray();
+
+            root.Add("capital", m_Capital.ToJSON());
+
+            for (int i = 1; i < m_Areas.Count; i++)
+            {
+                array.Add(m_Areas[i].ToJSON());
+            }
+
+            root.Add("areas", array);
+
+            return root;
+        }
+
+        public IEnumerator<Area> GetEnumerator()
+        {
+            return m_Areas.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return m_Areas.GetEnumerator();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Model/World/Map/World.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MS.Model.Map
+{
+    public class World : ModelElement, IEnumerable<Region>
+    {
+        protected List<Region> m_Regions;
+
+        protected int m_HorizontalSize;
+        protected int m_VerticalSize;
+        
+        public World(int horizontalSize, int verticalSize)
+        {
+            m_HorizontalSize    =   horizontalSize;
+            m_VerticalSize      =   verticalSize;
+            m_Regions           =   new List<Region>(horizontalSize * verticalSize);
+        }
+
+        protected void AddRegion(Region region, Vector2 boardPosition)
+        {
+            m_Regions[CalculateListIndex(boardPosition)] = region;
+        }
+
+        protected int CalculateListIndex(Vector2 boardPosition)
+        {
+            return CalculateListIndex((int)boardPosition.x, (int)boardPosition.y);
+        }
+
+        protected int CalculateListIndex(int x, int y)
+        {
+            return (x * m_HorizontalSize) + y;
+        }
+
+        public IEnumerator<Region> GetEnumerator()
+        {
+            return m_Regions.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return m_Regions.GetEnumerator();
+        }
+    }
+}
